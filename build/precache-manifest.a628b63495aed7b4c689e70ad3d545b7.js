9083793ed2f04bda5d95b6fd88ccd5c84763051e
Hanscau <hanscau@hotmail.com>
2020-06-13 23:19:19 +0800

    Build React UI v1

---
+++ build/precache-manifest.a628b63495aed7b4c689e70ad3d545b7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "841043d0c9b72f4f00ae3224294132a9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bd0a377ed5897d33bce7",
+    "url": "/static/css/main.a57146d7.chunk.css"
+  },
+  {
+    "revision": "67a991f04bf6484075e1",
+    "url": "/static/js/2.35134596.chunk.js"
+  },
+  {
+    "revision": "1a4f9806465c852a6982e21b4a6e096d",
+    "url": "/static/js/2.35134596.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bd0a377ed5897d33bce7",
+    "url": "/static/js/main.851227bb.chunk.js"
+  },
+  {
+    "revision": "13a494a5a7d2fb15e758",
+    "url": "/static/js/runtime-main.980fcacb.js"
+  }
+]);
